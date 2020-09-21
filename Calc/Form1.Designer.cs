0a01012fd96fd2c5bda155dbd99ba71e6d29df22
DEsimas <simasuh@gmail.com>
2020-09-21 15:55:32 +0300

    Uploading project

---
+++ Calc/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Calc
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.n1 = new System.Windows.Forms.TextBox();
+            this.n2 = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.Label();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.umn = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.step = new System.Windows.Forms.Button();
+            this.res = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cl = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // n1
+            // 
+            this.n1.Location = new System.Drawing.Point(12, 61);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(45, 20);
+            this.n1.TabIndex = 0;
+            // 
+            // n2
+            // 
+            this.n2.Location = new System.Drawing.Point(89, 60);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(45, 20);
+            this.n2.TabIndex = 1;
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Location = new System.Drawing.Point(6, 16);
+            this.Result.Name = "Result";
+            this.Result.Padding = new System.Windows.Forms.Padding(2);
+            this.Result.Size = new System.Drawing.Size(4, 17);
+            this.Result.TabIndex = 3;
+            // 
+            // plus
+            // 
+            this.plus.BackColor = System.Drawing.SystemColors.Control;
+            this.plus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.plus.Location = new System.Drawing.Point(63, 60);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(20, 20);
+            this.plus.TabIndex = 4;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = false;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.minus.Location = new System.Drawing.Point(63, 33);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(20, 21);
+            this.minus.TabIndex = 5;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // umn
+            // 
+            this.umn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.umn.Location = new System.Drawing.Point(63, 6);
+            this.umn.Name = "umn";
+            this.umn.Size = new System.Drawing.Size(20, 21);
+            this.umn.TabIndex = 6;
+            this.umn.Text = "*";
+            this.umn.UseVisualStyleBackColor = true;
+            this.umn.Click += new System.EventHandler(this.umn_Click);
+            // 
+            // del
+            // 
+            this.del.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.del.Location = new System.Drawing.Point(63, 86);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(20, 21);
+            this.del.TabIndex = 7;
+            this.del.Text = "/";
+            this.del.UseVisualStyleBackColor = true;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // step
+            // 
+            this.step.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.step.Location = new System.Drawing.Point(63, 113);
+            this.step.Name = "step";
+            this.step.Size = new System.Drawing.Size(20, 21);
+            this.step.TabIndex = 8;
+            this.step.Text = "^";
+            this.step.UseVisualStyleBackColor = true;
+            this.step.Click += new System.EventHandler(this.step_Click);
+            // 
+            // res
+            // 
+            this.res.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.res.Location = new System.Drawing.Point(140, 59);
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(20, 21);
+            this.res.TabIndex = 4;
+            this.res.Text = "=";
+            this.res.UseVisualStyleBackColor = true;
+            this.res.Click += new System.EventHandler(this.res_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Result);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.groupBox1.Location = new System.Drawing.Point(166, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(98, 94);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ответ";
+            // 
+            // cl
+            // 
+            this.cl.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cl.Location = new System.Drawing.Point(166, 14);
+            this.cl.Name = "cl";
+            this.cl.Size = new System.Drawing.Size(75, 23);
+            this.cl.TabIndex = 9;
+            this.cl.Text = "Очистить";
+            this.cl.UseVisualStyleBackColor = true;
+            this.cl.Click += new System.EventHandler(this.cl_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 145);
+            this.Controls.Add(this.cl);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.step);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.umn);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Name = "Main";
+            this.Text = "Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox n1;
+        private System.Windows.Forms.TextBox n2;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button umn;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.Button step;
+        private System.Windows.Forms.Button res;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button cl;
+    }
+}
+
---
+++ Calc/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class Main : Form
+    {
+        private double a, b;
+        private int ch = 0, key = 0;
+        private string back1, back2;
+
+        public Main()
+        {
+            InitializeComponent();
+            umn.BackColor = Color.White;
+            del.BackColor = Color.White;
+            plus.BackColor = Color.Blue;
+            minus.BackColor = Color.White;
+            step.BackColor = Color.White;
+            this.KeyPreview = true;
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Main_KeyUp);
+        }
+
+        private void result()
+        {
+            try
+            {
+                a = Convert.ToDouble(n1.Text);
+                b = Convert.ToDouble(n2.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка: неверный формат ввода");
+            }
+            switch (ch)
+            {
+                case 0:
+                    Result.Text = Convert.ToString(a + b);
+                    break;
+                case 1:
+                    Result.Text = Convert.ToString(a - b);
+                    break;
+                case 2:
+                    Result.Text = Convert.ToString(a * b);
+                    break;
+                case 3:
+                    if (b != 0)
+                    {
+                        Result.Text = Convert.ToString(a / b);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка: деление на 0");
+                    }
+                    break;
+                case 4:
+                    if ((float)((int)b) == b) //Целое
+                    {
+                        Result.Text = Convert.ToString(Math.Pow(a, b));
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка: степень должна быть целым числом.");
+                    }
+                    break;
+            }
+        }
+
+        private void umn_Click(object sender, EventArgs e)
+        {
+            ch = 2;
+            umn.BackColor = Color.Blue;
+            del.BackColor = Color.White;
+            plus.BackColor = Color.White;
+            minus.BackColor = Color.White;
+            step.BackColor = Color.White;
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            ch = 1;
+            umn.BackColor = Color.White;
+            del.BackColor = Color.White;
+            plus.BackColor = Color.White;
+            minus.BackColor = Color.Blue;
+            step.BackColor = Color.White;
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            ch = 0;
+            umn.BackColor = Color.White;
+            del.BackColor = Color.White;
+            plus.BackColor = Color.Blue;
+            minus.BackColor = Color.White;
+            step.BackColor = Color.White;
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            ch = 3;
+            umn.BackColor = Color.White;
+            del.BackColor = Color.Blue;
+            plus.BackColor = Color.White;
+            minus.BackColor = Color.White;
+            step.BackColor = Color.White;
+        }
+
+        private void res_Click(object sender, EventArgs e)
+        {
+            result();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void cl_Click(object sender, EventArgs e)
+        {
+            n1.Text = "";
+            n2.Text = "";
+            Result.Text = "";
+            ch = 0;
+            umn.BackColor = Color.White;
+            del.BackColor = Color.White;
+            plus.BackColor = Color.Blue;
+            minus.BackColor = Color.White;
+            step.BackColor = Color.White;
+        }
+
+
+        private void step_Click(object sender, EventArgs e)
+        {
+            ch = 4;
+            umn.BackColor = Color.White;
+            del.BackColor = Color.White;
+            plus.BackColor = Color.White;
+            minus.BackColor = Color.White;
+            step.BackColor = Color.Blue;
+        }
+        //Keys
+        private void Main_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Add)
+            {
+                umn.BackColor = Color.White;
+                del.BackColor = Color.White;
+                plus.BackColor = Color.Blue;
+                minus.BackColor = Color.White;
+                step.BackColor = Color.White;
+                ch = 0;
+                n1.Text = back1;
+                n2.Text = back2;
+            }
+            else if (e.KeyCode == Keys.Subtract)
+            {
+                umn.BackColor = Color.White;
+                del.BackColor = Color.White;
+                plus.BackColor = Color.White;
+                minus.BackColor = Color.Blue;
+                step.BackColor = Color.White;
+                ch = 1;
+                n1.Text = back1;
+                n2.Text = back2;
+            }
+            else if (e.KeyCode == Keys.Multiply)
+            {
+                umn.BackColor = Color.Blue;
+                del.BackColor = Color.White;
+                plus.BackColor = Color.White;
+                minus.BackColor = Color.White;
+                step.BackColor = Color.White;
+                ch = 2;
+                n1.Text = back1;
+                n2.Text = back2;
+            }
+            else if (e.KeyCode == Keys.Divide)
+            {
+                umn.BackColor = Color.White;
+                del.BackColor = Color.Blue;
+                plus.BackColor = Color.White;
+                minus.BackColor = Color.White;
+                step.BackColor = Color.White;
+                ch = 3;
+                n1.Text = back1;
+                n2.Text = back2;
+            }
+            else
+            {
+                back1 = n1.Text;
+                back2 = n2.Text;
+            }
+
+            /*if (e.KeyCode == Keys.)
+            {
+                if (ch == 4)
+                    ch = 1;
+                else
+                    ch += 1;
+            }
+            if (e.KeyCode == Keys.)
+            {
+                if (ch == 0)
+                    ch = 4;
+                else
+                    ch -= 1;
+            } */
+
+
+            if (e.KeyCode == Keys.Escape)
+                cl.PerformClick();
+            if (e.KeyCode == Keys.Delete)
+                 cl.PerformClick();
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (key == 0)
+                {
+                    n2.Focus();
+                    key = 2;
+                }
+                else if (key == 2)
+                {
+                    res.PerformClick();
+                    key = 1;
+                }
+                else if(key == 1)
+                {
+                    n1.Focus();
+                    key = 0;
+                }
+            }
+        }
+    }
+}
